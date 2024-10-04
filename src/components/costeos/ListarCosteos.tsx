d2e968b96fea791e2a9e99eb665e2bfefe935e5d
DavidRomeroC <romerocandelariodavid@gmail.com>
2024-10-03 21:52:01 -0600

    componente listarcosteos modificado

--- src/components/costeos/ListarCosteos.tsx
+++ src/components/costeos/ListarCosteos.tsx
@@ -10,12 +10,13 @@ const ListarCosteos = ({ costeos }: PropsListarCosteos) => {
 
     useEffect(() => { router.refresh() }, [])
 
+    console.log(costeos)
 
     return (
         <div className='w-4/5 h-10 border-2 self-center mt-10 ' >
             {
                 costeos.map((costeo: DataCosteos) => (
-                    <Link href={`costeos/${costeo.uuid.toString()}`} key={costeo.uuid} className='flex items-center w-full justify-around bg-red-400 rounded-lg h-10 my-4 font-bold text-xl ' >
+                    <Link href={`costeos/${costeo.uuid}`} key={costeo.uuid} className='flex items-center w-full justify-around bg-red-400 rounded-lg h-10 my-4 font-bold text-xl ' >
                         <h3 className='flex align-middle' > Fecha del viaje: {costeo.fecha} </h3>
                         <h3> Monto: ${costeo.monto} </h3>
                         <h2> Costea: {costeo.costea} </h2>
--- src/helpers/costeos/getDataCosteos.ts
+++ src/helpers/costeos/getDataCosteos.ts
@@ -1,6 +1,5 @@
 import dotenv from "dotenv"
 
-
 async function fetchSucursales() {
     dotenv.config()
     const apiCosteos = process.env.NEXT_PUBLIC_API_BASE_URL
